4aa27638f20ef9233c6df8e89ad1ae7729572c05
schaewo <schaewo@gmail.com>
2024-04-21 01:58:28 -0400

    restructured kali images

--- images/misc/kali/base/build.sh
+++ images/misc/kali/base/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-IMG_NAME=ctlabs/c9/base
-IMG_VERS=0.3
+IMG_NAME="ctlabs/kali/base"
+IMG_VERS=0.1
 
 docker build --rm -t ${IMG_NAME}:${IMG_VERS} -t ${IMG_NAME}:latest .
+
--- images/misc/kali/ctf/build.sh
+++ images/misc/kali/ctf/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMG_NAME="ctlabs/misc/kali"
+IMG_NAME="ctlabs/kali/ctf"
 IMG_VERS=0.1
 
 docker build --rm -t ${IMG_NAME}:${IMG_VERS} -t ${IMG_NAME}:latest .
