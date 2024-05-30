14bbad89fcd08ce10a7e78f861e772edb1a92b33
oxdeca <acedxo@oxdeca.com>
2024-05-29 21:00:26 -0400

    debian img: add gpg

--- images/debian/d11/qemu/files/qemu_init.sh
+++ images/debian/d11/qemu/files/qemu_init.sh
@@ -33,7 +33,7 @@ tmux new -d -s qemu
 
 tmux send-keys -t qemu "qemu-system-x86_64 -nodefaults -display none -m 256M -serial mon:stdio -smp 1   \
                          -boot c -drive file=/media/${IMG} ${ENABLE_KVM}                                \
-                         -drive file=/media/vda.qcow2 -vga virtio -vnc :1                               \
+                         -drive file=/media/vda.qcow2 -vga virtio                                       \
                          -virtfs local,path=/mnt,mount_tag=setup,readonly=on,security_model=passthrough \
                          -nic tap,ifname=ens0,br=net0,script=no,downscript=no                           \
                          -nic tap,ifname=ens1,br=net1,script=/root/if-up,downscript=/root/if-down" ENTER
