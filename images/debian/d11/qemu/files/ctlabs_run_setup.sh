f1ee839e0a34c0560e5e7953f0a0ced9ee5f3a0e
oxdeca <acedxo@oxdeca.com>
2024-08-08 01:15:40 -0400

    use iso image for transfering nic configuration

--- images/debian/d11/qemu/files/ctlabs_run_setup.sh
+++ images/debian/d11/qemu/files/ctlabs_run_setup.sh
@@ -3,10 +3,10 @@
 ip link add mgmt type vrf table 40
 ip link set mgmt up
 
-ip addr add 169.254.40.2/30 dev ens3
 ip link set ens3 master mgmt up
 
-ip vrf exec mgmt sshpass -p 'secret' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@169.254.40.1:/mnt/ctlabs_net_setup.sh /root/
-bash /root/ctlabs_net_setup.sh
+mount /dev/cdrom /mnt
+bash /mnt/ctlabs_net_setup.sh
+umount /mnt
 
 exit 0
--- images/debian/d11/qemu/files/qemu_init.sh
+++ images/debian/d11/qemu/files/qemu_init.sh
@@ -27,12 +27,45 @@ if [ -c /dev/kvm ]; then
   ENABLE_KVM="--enable-kvm"
 fi
 
+create_net_setup_script() {
+  local premac="52:54:00:12:34:"
+  local eth0_ip=$( ip -br addr ls eth0 | awk '{print $3}' )
+  local eth0_gw=$( ip -br route ls default vrf mgmt | awk '{print $3}' )
+  local eth1_ip=$( ip -br addr ls eth1 | awk '{print $3}' )
+  local eth1_gw=$( ip -br route ls default | awk '{print $3}' )
+cat > /mnt/ctlabs_net_setup.sh << EOF
+#!/bin/bash
+
+hostnamectl set-hostname ${HOSTNAME}
+
+
+# ens3
+ip addr add ${eth0_ip} dev ens3
+ip link set ens3 master mgmt mtu 1460 up
+ip link set ens3 address "${premac}$(openssl rand -hex 1)"
+ip route add default via ${eth0_gw} vrf mgmt
+
+# ens4
+ip addr add ${eth1_ip} dev ens4
+ip link set ens4 mtu 1460 up
+ip link set ens4 address "${premac}$(openssl rand -hex 1)"
+ip route add default via ${eth1_gw}
+
+echo '$(cat /etc/resolv.conf)' > /etc/resolv.conf
+EOF
+
+}
+
+create_net_setup_script
+mkisofs -r -o /tmp/${HOSTNAME}.iso /mnt/
+
 # start tmux session
 tmux new -d -s qemu
 
 tmux send-keys -t qemu "qemu-system-x86_64 -nodefaults -display none -m ${QEMU_MEM} -serial mon:stdio -smp 1 \
                          -boot c -drive file=/media/${IMG} ${ENABLE_KVM}                                     \
                          -drive file=/media/vda.qcow2 -vga virtio                                            \
+                         -drive file=/tmp/${HOSTNAME}.iso,media=cdrom                                        \
                          -nic tap,ifname=ens0,br=net0,script=no,downscript=no                                \
                          -nic tap,ifname=ens1,br=net1,script=/root/if-up,downscript=/root/if-down" ENTER
 
