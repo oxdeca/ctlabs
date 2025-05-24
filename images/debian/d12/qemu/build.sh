2b459609208aa58e1f4e8317632cc2674492b23f
oxdeca <acedxo@oxdeca.com>
2025-05-24 15:46:54 -0400

    deb12 qemu image

--- images/debian/d12/qemu/build.sh
+++ images/debian/d12/qemu/build.sh
@@ -31,7 +31,7 @@ create_qemu_img() {
   chroot ${MNTDIR} /usr/bin/systemctl enable ctlabs-net.service sshd-mgmt.service
   chroot ${MNTDIR} /bin/sh -c 'rm /etc/resolv.conf'
   chroot ${MNTDIR} /bin/sh -c 'echo "nameserver 1.1.1.1" > /etc/resolv.conf'
-  chroot ${MNTDIR} /bin/sh -c 'mknod -m0666 /dev/null c 1 3'
+  chroot ${MNTDIR} /bin/sh -c 'mknod /dev/null c 1 3 && chmod 0666 /dev/null'
   chroot ${MNTDIR} /bin/sh -c 'apt update && apt -y remove man-db'
   chroot ${MNTDIR} /bin/sh -c 'apt update && apt -y install openssh-server lvm2 fdisk nfs-kernel-server cloud-utils sshpass xterm gnupg'
   chroot ${MNTDIR} /bin/sh -c 'mv /bin/resize /usr/local/bin/ && apt -y remove xterm && apt -y clean && apt -y autoclean && apt -y autoremove'
